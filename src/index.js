7ac8a943565d016e026811b411613a7c89d44bb0
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-13 10:41:59 -0600

    added counter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Counter = () => {
+  return (
+    <div id='container'>
+      <div id='navbar'>
+        Counter.js
+      </div>
+      <div id='counter'>
+        <h1>0</h1>
+        <button>Increment</button>
+      </div>
+    </div>
+  )
+}
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('app'));
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
