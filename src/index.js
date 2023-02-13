71f639644747fed186a91a85ed2ce52765967747
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-13 10:53:02 -0600

    Added component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div id='container'>
       <div id='navbar'>
         Counter.js
       </div>
       <div id='counter'>
-        <h1>0</h1>
-        <button>Increment</button>
+        <h1>{ count }</h1>
+        <button onClick={increment}>Increment</button>
       </div>
     </div>
   )
